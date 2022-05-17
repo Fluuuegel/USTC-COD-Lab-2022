fdfe6d6374134588093cc22d3fc64159ff9c7376
root <root@VM3242-zyy.ustc.edu.cn>
2022-05-17 23:17:44 +0800

    branch prediction debug

--- lab6/program_counter.v
+++ lab6/program_counter.v
@@ -10,8 +10,9 @@ module program_counter(
     input jump,
 
     input [31:0] alu_out,
-    input PCWrite,
+    input fStall,
 
+    input [31:0] pce,
     output reg [31:0] pc,
     output reg [31:0] pcin,
     output [31:0] pc_plus4
@@ -21,9 +22,9 @@ module program_counter(
 always@(*) begin
     if(jump) pcin <= alu_out;
     else if(br && ~br_pred_e) pcin <= alu_out;  //预测不跳转但实际跳转
-    else if(~br && br_pred_e) pcin <= pc_plus4; //预测跳转但实际不跳转
+    else if(~br && br_pred_e) pcin <= pce + 4; //预测跳转但实际不跳转
     else if(br_pred_f) pcin <= br_pred_pc;    
-    else if(!PCWrite) pcin <= pc;  //idk if branch prediction has conflict with data hazard
+    else if(fStall) pcin <= pc;  //idk if branch prediction has conflict with data hazard
     else pcin <= pc_plus4;
 end
 
